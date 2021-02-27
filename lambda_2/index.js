b688ccf23307cd42bc6f9ba07776176f7cef034a
tyanko007 <herakuresurikki@gmail.com>
2021-02-27 10:23:36 +0900

    index.js change master code

--- lambda_2/index.js
+++ lambda_2/index.js
@@ -1,5 +1,6 @@
 // 1. The bugets and key in a new video file import to event object
 // 2. Anyone can change the read permission of any user's acl attribute to access the video file.
+/* 自分で書いたコード。なんか)が変とのエラーが返ってくる
 'use strict'
 
 var AWS = require("aws-sdk")
@@ -24,3 +25,28 @@ exports.handler = function(event, context, callback){
     }
   })
 }
+*/
+
+'use strict';
+
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+exports.handler = function(event, context, callback){
+    var message = JSON.parse(event.Records[0].Sns.Message);
+
+    var sourceBucket = message.Records[0].s3.bucket.name;
+    var sourceKey = decodeURIComponent(message.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+    var params = {
+        Bucket: sourceBucket,
+        Key: sourceKey,
+        ACL: 'public-read'
+    };
+
+    s3.putObjectAcl(params, function(err, data){
+        if (err) {
+            callback(err);
+        }
+    });
+};
